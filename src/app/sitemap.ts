8c47c8d658fceb754f8157886f2ac8db83ee4f2f
sarthak <sarthak.robo@gmail.com>
2024-12-19 11:06:31 +0530

    fix: build errors

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,1 +1,11 @@
 export const baseUrl = "https://sarthakmangla.com"
+
+export default function sitemap() {
+
+  const routes = [''].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes]
+}
