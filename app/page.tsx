0097120daccaf41b42d39768c57b9017cfbf5f38
sarthak <sarthak.robo@gmail.com>
2025-02-11 12:41:53 -0500

    add whatdidyoudo

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
         <br />
         <p>Last summer, I helped build AI systems to <Link href="https://bubble.io/blog/bubblecon-2024-bubble-assist-ai/">generate full-stack MVPs</Link> on <Link href="https://bubble.io/" >Bubble</Link>&apos;s visual DSL.</p>
         <br />
-        <p>A few other things I&apos;ve made include <Link href="https://boilerclasses.com/">BoilerClasses</Link>, <Link href="https://www.roamer.dev/">RoamerAI</Link>, <Link href="https://twitter.com/msarthak29/status/1749267405656687076">Pulse</Link>, <Link href="https://chromewebstore.google.com/detail/tempus/bpdhbpeecmmglmkjfmigehaebpndmceh">Tempus</Link>, and <Link href="https://www.producthunt.com/products/interview-checklist#interview-checklist">Interview Checklist</Link>.</p>
+        <p>A few other things I&apos;ve made include <Link href="https://boilerclasses.com/">BoilerClasses</Link>, <Link href="https://www.whatdidyoudo.dev/">What Did You Do</Link>, <Link href="https://www.roamer.dev/">RoamerAI</Link>, <Link href="https://twitter.com/msarthak29/status/1749267405656687076">Pulse</Link>, <Link href="https://chromewebstore.google.com/detail/tempus/bpdhbpeecmmglmkjfmigehaebpndmceh">Tempus</Link>, and <Link href="https://www.producthunt.com/products/interview-checklist#interview-checklist">Interview Checklist</Link>.</p>
         <br />
         <p>Outside of all this, I love a good pesto dish, roadtrips, minimalist design and whodunits. I also sometimes try to make <Link href="https://www.misbalanced.link/">generative sketches</Link>.</p>
       </section>
