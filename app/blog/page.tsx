82844c4401d06432f3608b8f7d20ba5c7c1a499b
sarthak <sarthak.robo@gmail.com>
2025-10-28 02:41:47 -0400

    skill issue blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -13,7 +13,10 @@ const cormorantGaramond = Cormorant_Garamond({
 export default function Page() {
   return (
     <section>
-      <h1 className="text-2xl md:text-3xl font-semibold mb-8" style={{ fontFamily: cormorantGaramond.style.fontFamily }}>Posts</h1>
+      <div className="mb-8">
+        <h1 className="text-2xl md:text-3xl font-semibold mb-2" style={{ fontFamily: cormorantGaramond.style.fontFamily }}>Posts</h1>
+        <p className="text-neutral-600">thoughts? rants? notes?</p>
+      </div>
       <BlogPosts />
     </section>
   )
