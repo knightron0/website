bd6f4e39d22890bd9651202ce1d521e8d06d3e90
sarthak <sarthak.robo@gmail.com>
2026-02-04 22:00:11 -0500

    update links

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
         <br /> 
         <p>I&apos;m also exploring robust self-supervised learning for speech enhancement at the <Link href="https://engineering.purdue.edu/HeinzLab/people">Heinz Lab</Link> + developing lightweight representation models for cardiac magnetic resonance (CMR) understanding at the <Link href="https://engineering.purdue.edu/cvirl">CVIRL lab</Link>.</p> 
         <br /> 
-        <p>A few other things I&apos;ve made include <Link href="https://boilerclasses.com/">BoilerClasses</Link>, <Link href="https://www.whatdidyoudo.dev/">What Did You Do</Link>, <Link href="https://www.roamer.dev/">RoamerAI</Link>, <Link href="https://twitter.com/msarthak29/status/1749267405656687076">Pulse</Link>, <Link href="https://chromewebstore.google.com/detail/tempus/bpdhbpeecmmglmkjfmigehaebpndmceh">Tempus</Link>, and <Link href="https://www.producthunt.com/products/interview-checklist#interview-checklist">Interview Checklist</Link>.</p> 
+        <p>A few other things I&apos;ve made include <Link href="https://boilerclasses.com/">BoilerClasses</Link>, <Link href="https://github.com/Divide-By-0/whatdidyoudo">What Did You Do</Link>, <Link href="https://twitter.com/msarthak29/status/1749267405656687076">Pulse</Link>, <Link href="https://chromewebstore.google.com/detail/tempus/bpdhbpeecmmglmkjfmigehaebpndmceh">Tempus</Link>, and <Link href="https://www.producthunt.com/products/interview-checklist#interview-checklist">Interview Checklist</Link>.</p> 
         <br />
         <p>In the past past, I was really into  <Link href="https://www.iarcs.org.in/inoi/2022/inoi2022/results_inoi2022.php#gold">competitive programming</Link> and <Link href="https://ioling.org/participants/IND">linguistics</Link>. </p>
         <br />
