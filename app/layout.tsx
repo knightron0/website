9abc650647a3e5fed7b45e5f6e91aaea4bf0a97d
sarthak <sarthak.robo@gmail.com>
2026-04-22 14:32:14 -0400

    update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
-import { GeistSans } from 'geist/font/sans'
+import { IBM_Plex_Sans } from 'next/font/google'
 import { baseUrl } from './sitemap'
 
 export const metadata: Metadata = {
@@ -40,6 +40,11 @@ export const metadata: Metadata = {
 
 const cx = (...classes) => classes.filter(Boolean).join(' ')
 
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+})
+
 export default function RootLayout({
   children,
 }: {
@@ -50,10 +55,10 @@ export default function RootLayout({
       lang="en"
       className={cx(
         'text-black bg-[#FCF6F1]',
-        GeistSans.variable
+        ibmPlexSans.variable
       )}
     >
-      <body className={`antialiased max-w-3xl mx-4 mt-8 lg:mx-auto ${GeistSans.className}`}>
+      <body className={`antialiased max-w-3xl mx-4 mt-8 lg:mx-auto ${ibmPlexSans.className}`}>
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
--- app/page.tsx
+++ app/page.tsx
@@ -19,15 +19,11 @@ export default function Page() {
         <div className="text-2xl md:text-3xl font-semibold pb-5" style={{ fontFamily: cormorantGaramond.style.fontFamily }}>Sarthak Mangla</div>
         <p>I study Computer Science (and some Math) at <Link href="https://www.cs.purdue.edu/">Purdue University</Link>.</p>
         <br />
-        <p>I spent this past summer breaking (and building) serving infrastructure and chasing Time-to-First Audio (TTFA) at <Link href="https://cartesia.ai">Cartesia AI</Link>.</p>
+        <p>I'm broadly interested in improving compute and data efficiency across the AI stack. My immediate goal is to build intuition for each layer so I can contribute meaningfully to both. </p>
         <br />
-        <p>My latest obsession is ML systems. A manifestation of that is <Link href="https://tensara.org">Tensara</Link>. Feel free to <Link href="https://github.com/tensara/tensara">leave a few PRs</Link> too while you're trying it out!</p>
-        <br /> 
-        <p>I&apos;m also exploring robust self-supervised learning for speech enhancement at the <Link href="https://engineering.purdue.edu/HeinzLab/people">Heinz Lab</Link> + developing lightweight representation models for cardiac magnetic resonance (CMR) understanding at the <Link href="https://engineering.purdue.edu/cvirl">CVIRL lab</Link>.</p> 
-        <br /> 
-        <p>A few other things I&apos;ve made include <Link href="https://boilerclasses.com/">BoilerClasses</Link>, <Link href="https://github.com/Divide-By-0/whatdidyoudo">What Did You Do</Link>, <Link href="https://twitter.com/msarthak29/status/1749267405656687076">Pulse</Link>, <Link href="https://chromewebstore.google.com/detail/tempus/bpdhbpeecmmglmkjfmigehaebpndmceh">Tempus</Link>, and <Link href="https://www.producthunt.com/products/interview-checklist#interview-checklist">Interview Checklist</Link>.</p> 
+        <p>So far, this has included large-scale serving infrastructure at <Link href="https://cartesia.ai">Cartesia AI</Link>, low-level GPU benchmarking work at <Link href="https://tensara.org">Tensara</Link>, research on <Link href="https://sarthakmangla.com/blog/bam/">optimizer dynamics</Link>, and self-supervised representation learning for healthcare at the <Link href="https://engineering.purdue.edu/cvirl">CVIRL</Link> and <Link href="https://engineering.purdue.edu/HeinzLab/people">Heinz</Link> labs.</p>
         <br />
-        <p>In the past past, I was really into  <Link href="https://www.iarcs.org.in/inoi/2022/inoi2022/results_inoi2022.php#gold">competitive programming</Link> and <Link href="https://ioling.org/participants/IND">linguistics</Link>. </p>
+        <p>Some of my past work includes <Link href="https://boilerclasses.com/">BoilerClasses</Link>, <Link href="https://github.com/Divide-By-0/whatdidyoudo">What Did You Do</Link>, <Link href="https://twitter.com/msarthak29/status/1749267405656687076">Pulse</Link>, <Link href="https://chromewebstore.google.com/detail/tempus/bpdhbpeecmmglmkjfmigehaebpndmceh">Tempus</Link>, and <Link href="https://www.producthunt.com/products/interview-checklist#interview-checklist">Interview Checklist</Link>. Before that, I was really into  <Link href="https://www.iarcs.org.in/inoi/2022/inoi2022/results_inoi2022.php#gold">competitive programming</Link> and <Link href="https://ioling.org/participants/IND">linguistics</Link>. </p>
         <br />
         <p>Outside of all this, I love a good pesto dish, roadtrips, minimalist design and whodunits. I also sometimes try to make <Link href="https://www.misbalanced.link/">generative sketches</Link>.</p>
       </section>
